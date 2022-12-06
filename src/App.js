c1d058665e7f7f811a1e3b2f7225147c2720fe97
Sairoden <odingandarosa@gmail.com>
2022-12-06 15:00:31 +0800

    Final project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,68 @@ import Tours from "./Tours";
 
 const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const handleRemoveTour = id => {
+    const newTours = tours.filter(tour => tour.id !== id);
+
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      setTours(data);
+      setLoading(false);
+    } catch (err) {
+      alert("Couldn't fetch");
+      setLoading(true);
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (tours.length === 0) {
+  }
+
+  return (
+    <>
+      {loading ? (
+        <main>
+          <Loading />
+        </main>
+      ) : (
+        <main>
+          {tours.length === 0 ? (
+            <div className="title">
+              <h2>no tours left</h2>
+              <button className="btn" onClick={() => fetchTours()}>
+                Refresh
+              </button>
+            </div>
+          ) : (
+            <Tours handleRemoveTour={handleRemoveTour} tours={tours} />
+          )}
+        </main>
+      )}
+    </>
+  );
 }
 
 export default App;
+
+// 07 fetch tours data
+
+// tours.length === 0 ?
+//       <main>
+//         <div className="title">
+//           <h2>no tours left</h2>
+//           <button onClick={() => fetchTours()}></button>
+//         </div>
+//       </main>
--- src/Tours.js
+++ src/Tours.js
@@ -1,7 +1,25 @@
 import React from "react";
 import Tour from "./Tour";
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({ tours, handleRemoveTour }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="underline"></div>
+        <div>
+          {tours.map(tour => {
+            return (
+              <Tour
+                handleRemoveTour={handleRemoveTour}
+                key={tour.id}
+                {...tour}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
